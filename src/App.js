3fbbfd03bf28de0c27ffe0cd42dfcdcca556c9e3
Pritam Sarkar <001pritam2012@gmail.com>
2021-10-21 18:12:20 +0530

    Add MiniDrawer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import './App.css';
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import {Button, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
-
-import {CLIENTS, EVENTS} from "./Constants";
 
 import {
     BrowserRouter as Router,
@@ -17,27 +10,15 @@ import {
 import EventForm from "./pages/EventForm";
 import EventList from "./pages/EventList";
 import Login from "./pages/Login";
+import MiniDrawer from "./components/NavBar";
 
 function App() {
     return (
         <Router>
             <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Login</Link>
-                        </li>
-                        <li>
-                            <Link to="/eventform">EventForm</Link>
-                        </li>
-                        <li>
-                            <Link to="/eventlist">EventList</Link>
-                        </li>
-                    </ul>
-                </nav>
+                <MiniDrawer />
 
-                {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
+
                 <Switch>
                     <Route path="/eventform">
                         <EventForm />
@@ -53,86 +34,5 @@ function App() {
         </Router>
     );
 }
-
-// const Item = styled(Paper)(({ theme }) => ({
-//     ...theme.typography.body2,
-//     padding: theme.spacing(1),
-//     textAlign: 'center',
-//     color: theme.palette.text.secondary,
-// }));
-//
-// function App() {
-//
-//     const [selectedClient, setSelectedClient] = React.useState('');
-//     const [selectedEvent, setSelectedEvent] = React.useState('');
-//
-//     const handleClientChange = (event) => {
-//         setSelectedClient(event.target.value);
-//     };
-//
-//     const handleEventChange = (event) => {
-//         setSelectedEvent(event.target.value);
-//     };
-//
-//     return (
-//         <div className="App">
-//             <Box
-//                 className="mui-box"
-//                 sx={{
-//                     display: 'flex',
-//                     flexGrow: 1,
-//                     p: 4,
-//                 }}
-//             >
-//                 <Grid container spacing={2}>
-//                     <Grid item xs={4}>
-//                         <Item>
-//                             <FormControl fullWidth>
-//                                 <InputLabel id="client-select-label">Client</InputLabel>
-//                                 <Select
-//                                     labelId="client-select-label"
-//                                     id="client-select"
-//                                     value={selectedClient}
-//                                     label="Client"
-//                                     onChange={handleClientChange}
-//                                 >
-//                                     {CLIENTS.map(client => (
-//                                         <MenuItem value={client.code}>{client.name}</MenuItem>
-//                                     ))}
-//                                 </Select>
-//                             </FormControl>
-//                         </Item>
-//                     </Grid>
-//                     <Grid item xs={4}>
-//                         <Item>
-//                             <FormControl fullWidth>
-//                                 <InputLabel id="event-select-label">Event</InputLabel>
-//                                 <Select
-//                                     labelId="event-select-label"
-//                                     id="event-select"
-//                                     value={selectedEvent}
-//                                     label="event"
-//                                     onChange={handleEventChange}
-//                                 >
-//                                     {EVENTS.map(event => (
-//                                         <MenuItem value={event.code}>{event.name}</MenuItem>
-//                                     ))}
-//                                 </Select>
-//                             </FormControl>
-//                         </Item>
-//                     </Grid>
-//                     <Grid item xs={4}>
-//                         <Item>xs=4</Item>
-//                     </Grid>
-//                     <Grid item xs={8}>
-//                         <Item>xs=8</Item>
-//                     </Grid>
-//                 </Grid>
-//             </Box>
-//
-//             <Button variant="contained">SUBMIT</Button>
-//         </div>
-//     );
-// }
 
 export default App;
--- src/pages/EventForm.js
+++ src/pages/EventForm.js
@@ -1,11 +1,89 @@
-import * as React from 'react';
-
 import './EventForm.css';
 
+import * as React from 'react';
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import {Button, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+import {CLIENTS, EVENTS} from "../Constants";
+import {styled} from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+
 function EventForm() {
+    const [selectedClient, setSelectedClient] = React.useState('');
+    const [selectedEvent, setSelectedEvent] = React.useState('');
+
+    const handleClientChange = (event) => {
+        setSelectedClient(event.target.value);
+    };
+
+    const handleEventChange = (event) => {
+        setSelectedEvent(event.target.value);
+    };
+
+    const Item = styled(Paper)(({ theme }) => ({
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    }));
+
     return (
         <div className="EventForm">
-            EventForm
+            <Box
+                className="mui-box"
+                sx={{
+                    display: 'flex',
+                    flexGrow: 1,
+                    p: 4,
+                }}
+            >
+                <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                        <Item>
+                            <FormControl fullWidth>
+                                <InputLabel id="client-select-label">Client</InputLabel>
+                                <Select
+                                    labelId="client-select-label"
+                                    id="client-select"
+                                    value={selectedClient}
+                                    label="Client"
+                                    onChange={handleClientChange}
+                                >
+                                    {CLIENTS.map(client => (
+                                        <MenuItem value={client.code}>{client.name}</MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </Item>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Item>
+                            <FormControl fullWidth>
+                                <InputLabel id="event-select-label">Event</InputLabel>
+                                <Select
+                                    labelId="event-select-label"
+                                    id="event-select"
+                                    value={selectedEvent}
+                                    label="event"
+                                    onChange={handleEventChange}
+                                >
+                                    {EVENTS.map(event => (
+                                        <MenuItem value={event.code}>{event.name}</MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </Item>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Item>xs=4</Item>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Item>xs=8</Item>
+                    </Grid>
+                </Grid>
+            </Box>
+
+            <Button variant="contained">SUBMIT</Button>
         </div>
     );
 }
